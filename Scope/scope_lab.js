915f391e1d86c4274256267d7e33dff074644bc6
karenbis <bisama.karen@gmail.com>
2025-11-02 15:45:14 -0500

    primeras variables

---
+++ Scope/scope_lab.js
@@ -0,0 +1,10 @@
+// Ámbito global
+var globalVar = "Soy una variable global";
+let globalLet = "También soy global, pero con ámbito de let";
+const globalConst = "Soy una constante global";
+{
+// Ámbito de bloque
+var blockVar = "Soy un var con ámbito de bloque";
+let blockLet = "Soy un let con ámbito de bloque";
+const blockConst = "Soy un const con ámbito de bloque";
+}
