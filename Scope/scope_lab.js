959bb67ae58b036f9414b37f71c0700ec6615a6a
karenbis <bisama.karen@gmail.com>
2025-11-02 15:46:54 -0500

    alcance global

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -8,3 +8,8 @@ var blockVar = "Soy un var con ámbito de bloque";
 let blockLet = "Soy un let con ámbito de bloque";
 const blockConst = "Soy un const con ámbito de bloque";
 }
+
+// Alcance global
+console.log(globalVar); // Salida: "Soy una variable global"
+console.log(globalLet); // Salida: "También soy global, pero con alcance de let"
+console.log(globalConst); // Salida: "Soy una constante global"
