a4cd06d500aa9ac0aadf37a187e3cf75d70bfac4
karenbis <bisama.karen@gmail.com>
2025-11-08 16:02:08 -0500

    ejercicio grocery store

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,4 +10,19 @@ let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
- 
+//Ejercicio de grocery store
+
+let amount1;
+let amount2;
+let amount3;
+
+function calculatetotalexpense() {
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+    let gastos = amount1 + amount2 + amount3;
+
+    document.getElementById('resultexpense').innerText = `El total gastado es de: ${gastos}`;
+}
+   
